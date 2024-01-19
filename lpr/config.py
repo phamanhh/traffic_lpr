dc5b81acbd5dc89bfe83f53bb5372ac3e8f62d20
phamanhh <phamthienanh8818@gmail.com>
2024-01-19 20:58:31 +0700

    a1

--- lpr/config.py
+++ lpr/config.py
@@ -1,11 +1,11 @@
 URI = f"mongodb+srv://anh:123@cluster0.drbzmts.mongodb.net/"
 
 firebase_config = {
-  "apiKey": "AIzaSyAlqF1_JPWxAPrcOKSSpjveEBks2aaGKso",
-  "authDomain": "lpr-data-9983a.firebaseapp.com",
-  "projectId": "lpr-data-9983a",
-  "storageBucket": "lpr-data-9983a.appspot.com",
+  "apiKey": "AIzaSyCODqolrMr_nkxSXP43NEDjnv-QB2P8BqE",
+  "authDomain": "lpr-traffic.firebaseapp.com",
+  "projectId": "lpr-traffic",
+  "storageBucket": "lpr-traffic.appspot.com",
   "serviceAccout": "service_account.json"
 }
 
-PRIVATE_KEY = "PhamThienAnh"
+PRIVATE_KEY = "PhamAnh"
--- traffic/src/firebase/config.jsx
+++ traffic/src/firebase/config.jsx
@@ -1,13 +1,12 @@
 import firebase from 'firebase'
 
 const firebaseConfig = {
-  "apiKey": "AIzaSyAlqF1_JPWxAPrcOKSSpjveEBks2aaGKso",
-  "authDomain": "lpr-data-9983a.firebaseapp.com",
-  "projectId": "lpr-data-9983a",
-  "storageBucket": "lpr-data-9983a.appspot.com",
+  "apiKey": "AIzaSyCODqolrMr_nkxSXP43NEDjnv-QB2P8BqE",
+  "authDomain": "lpr-traffic.firebaseapp.com",
+  "projectId": "lpr-traffic",
+  "storageBucket": "lpr-traffic.appspot.com",
   "serviceAccout": "service_account.json"
 };
-
 firebase.initializeApp(firebaseConfig); 
 const storage = firebase.storage();
 
